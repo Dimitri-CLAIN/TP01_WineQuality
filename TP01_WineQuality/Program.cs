1d3b01c69e98357f6d2b2aa39520ec4535e7ccff
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-15 15:21:00 -0500

    Init projet :tada:

---
+++ TP01_WineQuality/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP01_WineQuality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
