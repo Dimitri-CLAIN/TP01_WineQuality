349d12f4de91412477b0f64188a4d3253471ef47
Pandatec <louis.vandenbosssche@epitech.eu>
2022-03-06 11:33:13 -0500

    [ADD] forget file

---
+++ TP01_WineQuality/Wine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP01_WineQuality
+{
+    internal class Wine
+    {
+        private float[] features;
+        float[] Features 
+        { 
+            set { features = value; }
+            get { return features; }
+        }
+
+        private int label;
+        int Label
+        {
+            set { label = value; }
+            get { return label; }
+        }
+        void PrintInfo() {
+            
+        }
+    }
+}
