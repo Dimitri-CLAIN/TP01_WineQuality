d955bcc265ac67f48b2845ca00172c5a34b4d960
Pandatec <louis.vandenbosssche@epitech.eu>
2022-03-08 12:58:56 -0500

    [FIX] modify of Matrix and accuracy for evaluate

--- TP01_WineQuality/KNN.cs
+++ TP01_WineQuality/KNN.cs
@@ -105,33 +105,33 @@ namespace TP01_WineQuality
             }
         }
         float EuclideanDistance(Wine first_sample, Wine second_sample) {
-            double res = 0;
+            float res = 0;
             for(int i = 0; i <= 3; i++) {
-                double tmp = first_sample.Features[i] - second_sample.Features[i];
-                double value = Math.Pow(tmp, 2);
+                float tmp = first_sample.Features[i] - second_sample.Features[i];
+                float value = (float)Math.Pow(tmp, 2);
                 res += value;
             }
-            res = Math.Sqrt(res);
-            return (float)res;
+            res = (float)Math.Sqrt(res);
+            return res;
         }
         public int Predict(string filename_sample_csv) {
             int res = 0;
             Wine sample = ImportOneSample(filename_sample_csv);
             List<float> distances = new List<float>();
-            List<int> expert_label = new List<int>();
+            List<int> labels = new List<int>();
 
             foreach(Wine wine in this.TrainList) {
                 distances.Add(EuclideanDistance(sample, wine));
-                expert_label.Add(wine.Label);
+                labels.Add(wine.Label);
             }
             if (this.sort_algorithm == 1) {
-                ShellSort(distances, expert_label);
+                ShellSort(distances, labels);
             } else if (this.sort_algorithm == 2) {
-                SelectionSort(distances, expert_label);
+                SelectionSort(distances, labels);
             } else {
                 throw new Exception("error valie of sort algorithm");
             }
-            res = Vote(expert_label);
+            res = Vote(labels);
             Console.WriteLine("[ {0} ] Prediction by model -> {1} | by expert -> {2}", filename_sample_csv, res, sample.Label);
             return res;
         }
@@ -149,40 +149,63 @@ namespace TP01_WineQuality
         }
         public void ConfusionMatrix(List<int> predicted_labels, List<int> expert_labels)
         {
-            Console.WriteLine("\t\t|\t3\t|\t6\t|\t9\t");
-            for (int i = 3; i <= 9; i += 3) {
-                Console.WriteLine($"{i}\t |\t {isInArray(i, 3, predicted_labels)}\t |\t {isInArray(i, 6, predicted_labels)}\t |\t {isInArray(i, 9, predicted_labels)}\t");
+            int tt = 0, ts = 0, tn = 0, st = 0, ss = 0, sn = 0, nt = 0, ns = 0, nn = 0;
+            for (int i = 0; i < predicted_labels.Count ; i ++) {
+                if (predicted_labels[i] == 3 && expert_labels[i] == 3)
+                    tt++;
+                if (predicted_labels[i] == 3 && expert_labels[i] == 6)
+                    ts++;
+                if (predicted_labels[i] == 3 && expert_labels[i] == 9)
+                    tn++;
+                if (predicted_labels[i] == 6 && expert_labels[i] == 3)
+                    st++;
+                if (predicted_labels[i] == 6 && expert_labels[i] == 6)
+                    ss++;
+                if (predicted_labels[i] == 6 && expert_labels[i] == 9)
+                    sn++;
+                if (predicted_labels[i] == 9 && expert_labels[i] == 3)
+                    nt++;
+                if (predicted_labels[i] == 9 && expert_labels[i] == 6)
+                    ns++;
+                if (predicted_labels[i] == 9 && expert_labels[i] == 9)
+                    nn++;
             }
+            Console.WriteLine("\t\t|\t3\t|\t6\t|\t9\t");
+            Console.WriteLine($"{3}\t |\t {tt}\t |\t {ts}\t |\t {tn}\t");
+            Console.WriteLine($"{6}\t |\t {st}\t |\t {ss}\t |\t {sn}\t");
+            Console.WriteLine($"{9}\t |\t {nt}\t |\t {ns}\t |\t {nn}\t");
         }
         public float Evaluate(string filename_test_set_csv) {
             float res = 0;
             List<Wine> testLists = ImportAllSamples(filename_test_set_csv);
             List<float> distances = new List<float>();
-            List<int> expert_labels = new List<int>();
+            List<int> labels = new List<int>();
             List<int> knn = new List<int>();
             foreach(Wine sample in testLists) {
+                distances.Clear();
+                labels.Clear();
                 foreach(Wine wine in this.TrainList) {
                     distances.Add(EuclideanDistance(sample, wine));
-                    expert_labels.Add(wine.Label);
+                    labels.Add(wine.Label);
                 }
                 if (this.sort_algorithm == 1) {
-                    ShellSort(distances, expert_labels);
+                    ShellSort(distances, labels);
                 } else if (this.sort_algorithm == 2) {
-                    SelectionSort(distances, expert_labels);
+                    SelectionSort(distances, labels);
                 } else {
                     throw new Exception("error valie of sort algorithm");
                 }
-                foreach(var dis in distances) {
-                    Console.WriteLine(dis);
-                }
-                knn.Add(Vote(expert_labels));
-            }
-            for (int i = 0; i < knn.Count(); i++) {
-                if (knn[i] == expert_labels[i])
+                knn.Add(Vote(labels));
+                if (Vote(labels) == sample.Label) {
                     res += 1;
+                }
             }
+            // for (int i = 0; i < knn.Count ; i ++) {
+            //     Console.WriteLine("{0} {1}", knn[i], labels[i]);
+            //     Console.WriteLine();
+            // }
             res = (res / knn.Count()) * 100;
-            ConfusionMatrix(knn, expert_labels);
+            ConfusionMatrix(knn, labels);
             Console.WriteLine();
             Console.WriteLine("Classification Accuracy -> {0} %", res);
             return res;
